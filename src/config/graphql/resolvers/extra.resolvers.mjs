bcbcca207888b9d0a9dd6b5e7ad763bd7406977b
Étienne S <sauvage-etienne@orange.fr>
2022-06-21 17:34:24 +0200

    Fix dupplicate Mutation on extra.resolvers.js

--- src/config/graphql/resolvers/extra.resolvers.mjs
+++ src/config/graphql/resolvers/extra.resolvers.mjs
@@ -4,9 +4,9 @@ const extraRepository = new ExtraRepository();
 
 export default {
   Query: {
-    Categories: () => "Categories",
-    Restrictions: () => "Restrictions",
-    Badges: () => "Badges",
+    Categories: () => 'Categories',
+    Restrictions: () => 'Restrictions',
+    Badges: () => 'Badges',
   },
   Mutation: {
     createBadge: (parent, { input }, ctx, info) => {
@@ -14,12 +14,6 @@ export default {
     },
     createCategory: (parent, args, ctx, info) => {
       return extraRepository.createCategory(args);
-    },
-  },
-
-  Mutation: {
-    createBadge: (parent, { input }, ctx, info) => {
-      return extraRepository.createBadge(input);
     },
   },
 };
