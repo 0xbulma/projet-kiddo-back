b25e1ee1cfac5d8f72a8e7ac70fbb9d15c48cfb0
Ben A <benjamin.anoufa@gmail.com>
2022-06-21 17:55:10 +0200

    Event repo & mutation créés

--- src/config/graphql/resolvers/event.resolvers.mjs
+++ src/config/graphql/resolvers/event.resolvers.mjs
@@ -1,5 +1,33 @@
+import EventRepository from '../../mongo/repository/EventRepository.mjs';
+import getFields from '../../../utils/getFields.mjs';
+
+const eventRepository = new EventRepository();
+
 export default {
   Query: {
-    Events: () => 'Events',
+    events: (parent, args, context, info) => {
+      const fields = getFields(info);
+      return eventRepository.getEvents(fields);
+    },
+    event: (parent, { id }, context, info) => {
+      const fields = getFields(info);
+      return eventRepository.getEvent(id, fields);
+    },
+  },
+
+  Mutation: {
+    createEvent: (parent, { input }, ctx, info) => {
+      const fields = getFields(info);
+      return eventRepository.createEvent(input, fields);
+    },
+
+    updateEvent: (parent, { id, input }, ctx, info) => {
+      const fields = getFields(info);
+      return eventRepository.updateEvent(id, input, fields);
+    },
+
+    deleteEvent: (parent, { id }, ctx, info) => {
+      return eventRepository.deleteEvent(id);
+    },
   },
 };
