0fa79fe469da6ffd613aa1582b1fbcd456bc6e34
Sidi ESSAADOUNI <essaadouni.s@gmail.com>
2022-06-21 17:38:05 +0200

    create an article in database (a corriger)

--- src/config/graphql/resolvers/article.resolvers.mjs
+++ src/config/graphql/resolvers/article.resolvers.mjs
@@ -1,5 +1,16 @@
+import ArticleRepository from '../../mongo/repository/ArticleRepository.mjs';
+
+const articleRepository = new ArticleRepository();
+
 export default {
   Query: {
     Articles: () => 'Articles',
+  },
+  
+  Mutation: {
+    createArticle: (parent, { articleInput }, ctx, info) => {
+      const createdArticle = articleRepository.createArticle(articleInput);
+      return createdArticle;
+    }
   },
 };
