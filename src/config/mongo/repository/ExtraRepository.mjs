b8a17b5438fe7376fba33e7b716f0a9d30914f50
Étienne S <sauvage-etienne@orange.fr>
2022-06-22 14:45:59 +0200

    Fix article input mutation + start refacto extra

--- src/config/mongo/repository/ExtraRepository.mjs
+++ src/config/mongo/repository/ExtraRepository.mjs
@@ -1,9 +1,20 @@
 import * as extraSchema from '../models/extra.model.mjs';
 
+const EXTRA_TYPES = {
+  CATEGORY: 0,
+  RESTRICTION: 1,
+  SIGNALMENT: 2,
+  REACTION: 3,
+};
+
 export default class ExtraRepository {
   //========
   // Badges
   //========
+  async getAllBadges() {
+    return await extraSchema.badgeSchema.find();
+  }
+
   async createBadge(badgeInput) {
     return await extraSchema.badgeSchema.create(badgeInput);
   }
@@ -19,6 +30,10 @@ export default class ExtraRepository {
   //============
   //  Categorie
   //============
+  async getAllCategory() {
+    return await extraSchema.categorySchema.find();
+  }
+
   async createCategory(name) {
     return await extraSchema.categorySchema.create({ name });
   }
@@ -33,6 +48,10 @@ export default class ExtraRepository {
   //==============
   //  Restriction
   //==============
+  async getAllRestriction() {
+    return await extraSchema.restrictionSchema.find();
+  }
+
   async createRestriction(name) {
     return await extraSchema.restrictionSchema.create({ name });
   }
@@ -73,5 +92,21 @@ export default class ExtraRepository {
 
   async removeReaction(_id) {
     return await extraSchema.reactionSchema.findByIdAndRemove({ _id });
+  }
+
+  //====================
+  async createExtra(type, value) {
+    switch (type) {
+      case EXTRA_TYPES.CATEGORY:
+        break;
+      case EXTRA_TYPES.SIGNALMENT:
+        break;
+      case EXTRA_TYPES.SIGNALMENT:
+        break;
+      case EXTRA_TYPES.REACTION:
+        break;
+      default:
+        throw new Error('Wrong ', type, ' not defined in EXTRA_TYPES');
+    }
   }
 }
