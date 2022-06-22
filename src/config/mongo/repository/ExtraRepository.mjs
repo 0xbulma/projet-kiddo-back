c7f553c4cec1f540ba62caeb3fe1eb1144e61a84
JimmyDmtrx <96940667+JimmyDmtrx@users.noreply.github.com>
2022-06-22 11:13:39 +0200

    mutation badge - category - restriction
    create modify and delete

--- src/config/mongo/repository/ExtraRepository.mjs
+++ src/config/mongo/repository/ExtraRepository.mjs
@@ -1,11 +1,49 @@
 import * as extraSchema from "../models/extra.model.mjs";
 
 export default class ExtraRepository {
+  //
+  // Création de badges
   async createBadge(badgeInput) {
     return await extraSchema.badgeSchema.create(badgeInput);
   }
-  
+  async modifyBadge(_id, modifyBadgeInput) {
+    return await extraSchema.badgeSchema.findByIdAndUpdate(
+      _id,
+      modifyBadgeInput,
+      { new: true }
+    );
+  }
+  async removeBadge(_id) {
+    return await extraSchema.badgeSchema.findByIdAndRemove({ _id });
+  }
+
+  //  Création / modif / remove Category
   async createCategory(name) {
-    return await extraSchema.categorySchema.create(name);
+    return await extraSchema.categorySchema.create({ name });
+  }
+  async modifyCategory(_id, name) {
+    return await extraSchema.categorySchema.findByIdAndUpdate(
+      _id,
+      { name },
+      { new: true }
+    );
+  }
+  async removeCategory(_id) {
+    return await extraSchema.categorySchema.findByIdAndRemove({ _id });
+  }
+
+  //  Création / modif / remove restriction
+  async createRestriction(name) {
+    return await extraSchema.restrictionSchema.create({ name });
+  }
+  async modifyRestriction(_id, name) {
+    return await extraSchema.restrictionSchema.findByIdAndUpdate(
+      _id,
+      { name },
+      { new: true }
+    );
+  }
+  async removeRestriction(_id) {
+    return await extraSchema.restrictionSchema.findByIdAndRemove({ _id });
   }
 }
