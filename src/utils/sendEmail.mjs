a33212aef6dea391958aace83f270d82fd2ea610
Étienne S <sauvage-etienne@orange.fr>
2022-07-05 10:59:08 +0200

    Fix adress

--- src/utils/sendEmail.mjs
+++ src/utils/sendEmail.mjs
@@ -5,9 +5,9 @@ async function sendEmail(resetToken, emailTo) {
   // Only needed if you don't have a real mail account for testing
   let testAccount = await nodemailer.createTestAccount();
 
-   // create reusable transporter object using the default SMTP transport
-   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -24,18 +24,18 @@ async function sendEmail(resetToken, emailTo) {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "test@gmail.com", // list of receivers
+    from: '"Fred Foo 👻" <foo@example.com>', // sender adress
+    to: 'test@gmail.com', // list of receivers
     subject: "Kiddo : création d'un nouveau mot de passe", // Subject line
     text: `Lien de récupartion de mot de passe: /https://kiddo-front-la-passerelle.netlify.app/recoverpassowrd?token=${resetToken}`, // plain text body
     html: `<b>Lien de récupartion de mot de passe: /https://kiddo-front-la-passerelle.netlify.app/recoverpassowrd?token=${resetToken}</b>`, // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
+  console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
