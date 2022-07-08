d22372144deedaac8f6a93cf6ecfd6c55124adb8
Étienne S <sauvage-etienne@orange.fr>
2022-07-09 00:05:41 +0200

    Add DisconnectUser method
    Need to send same cookie with expire "now" for remove HttpOnly because we can"t access with Front JS

--- src/config/graphql/resolvers/user/user.connection.resolvers.mjs
+++ src/config/graphql/resolvers/user/user.connection.resolvers.mjs
@@ -1,14 +1,13 @@
 import bcrypt from 'bcryptjs';
 import { GraphQLError } from 'graphql';
 import jwt from 'jsonwebtoken';
-
+import { getUserTokenByCookies } from '../../../../utils/constant.mjs';
 import UserRepository from '../../../mongo/repository/UserRepository.mjs';
 const userRepository = new UserRepository();
 
 export const USER_CONNECTION = async (parent, { email, password }, { req, res }) => {
   const user = await userRepository.getByEmail(email);
   if (user == null) return new GraphQLError('Utiliateur introuvable !');
-  console.log('user connection')
   try {
     if (await bcrypt.compare(password, user.password)) {
       const cookie_options = {
@@ -17,7 +16,6 @@ export const USER_CONNECTION = async (parent, { email, password }, { req, res })
         maxAge: 1000 * 60 * 60 * 24 * 7, //Store for 7 days
       };
       const token = jwt.sign({ _id: user._id, email }, process.env.JWT_TOKEN_SECRET, { expiresIn: 1000 * 60 * 60 * 24 * 7 });
-      console.log(token);
       res.cookie('authorization', 'Bearer ' + token, cookie_options);
       return user;
     } else {
@@ -27,4 +25,19 @@ export const USER_CONNECTION = async (parent, { email, password }, { req, res })
     console.log('error', e);
     return new GraphQLError(e);
   }
+};
+
+export const DISCONNECT_USER = async (parent, { _id }, { req, res }) => {
+  const user = await userRepository.getById(_id);
+  if (user == null) return new GraphQLError('Utiliateur introuvable !');
+
+  const token = getUserTokenByCookies(req);
+  if (token == null) return new GraphQLError("Token de l'utilisateur introuvable !");
+  const cookie_options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'PROD',
+    maxAge: 1,
+  };
+  res.cookie('authorization', 'Bearer ' + token, cookie_options);
+  return user;
 };
